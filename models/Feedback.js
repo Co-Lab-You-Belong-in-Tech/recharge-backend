4e78d26a11dbe0baaa5afcc4553502fce701f63e
mihail <mihail.soltan@protonmail.ch>
2021-10-18 19:34:14 +0200

    modified feedback schema according to original design

--- models/Feedback.js
+++ models/Feedback.js
@@ -4,7 +4,7 @@ const FeedbackSchema = new mongoose.Schema(
     {
         mark: {required: false, type: Number, min: 0, max: 3},
         comment: {required: false, type: String},
-        label: {required: false, type:String, enum : ['very unsatisfied', 'unsatisfied', 'satisfied', 'very satisfied'], default: 'satisfied'}
+        label: {required: false, type:String, enum : ['very unhelpful', 'unhelpful', 'helpful', 'very helpful'], default: 'helpful'}
     }
 )
 
